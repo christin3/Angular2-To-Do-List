45713bf7f77ae4a2dff491df18cdfbd6dcb4c898
Christine <baba.christine@gmail.com>
2017-01-14 13:28:18 -0600

    --prod

---
+++ src/app/create-item-form/create-item-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {toDoService} from '../services/to-do.service';
+
+@Component({
+  selector: 'create-item-form',
+  templateUrl: './create-item-form.component.html',
+  styleUrls: ['./create-item-form.component.css']
+})
+export class CreateItemFormComponent implements OnInit {
+
+  constructor(private toDoService: toDoService) { }
+
+  public itemTitle: string;
+  public itemInfo: string;
+
+  //this will be used to post a new to do is to-do items array
+  postItem(){
+    //new Item variable getting this info from the to-so.service.ts file
+    var newItem = {
+      'title': this.itemTitle,
+      'info': this.itemInfo,
+      'taskNumber': this.toDoService.items.length +1,
+      'completed': false
+    }
+    this.toDoService.items.push(newItem);
+    this.itemTitle ="";
+    this.itemInfo ="";
+    console.log(this.toDoService.items);
+  }
+
+  ngOnInit() {
+  }
+
+}
